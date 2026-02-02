c4dd429b7e0f873db9fd5e2128df90a0fe559c06
jliang602 <jliang602@gmail.com>
2026-02-02 10:52:09 -0500

    Updated 2/2/2026 at 10:52

--- Traffic_Light/Traffic_Light/main.c
+++ Traffic_Light/Traffic_Light/main.c
@@ -13,9 +13,12 @@
  *  PORTL.0 for push button
  *
  * Hardware Output:
- *  PORTA.0 for red light
- *  PORTA.1 for yellow light
- *	PORTA.2 for green light
+ *  PORTA.0 for traffic light red
+ *  PORTA.1 for traffic light yellow
+ *	PORTA.2 for traffic light green
+ *  PORTG.0 for red light on multicolor LED
+ *  PORTG.1 for green light on multicolor LED
+ *	PORTG.2 for blue light on multicolor LED
  */ 
 
 
@@ -30,9 +33,8 @@
 #define red_light					0x01
 #define yellow_light				0x02
 #define green_light					0x04
-#define dont_walk_signal		
-#define walk_signal
-#define signal_activated_flag		0x00
+#define dont_walk_signal			0x03		
+#define walk_signal					0x07
 
 
 
@@ -45,21 +47,32 @@ int main(void)
 	DDRA = DDRA | (1<<PA2) | (1<<PA1) | (1<<PA0);		// Ports A.0, A.1, and A.2 as output to control traffic light
 	PORTA = PORTA & ~((1<<PA2) | (1<<PA1) | (1<<PA0));		// Ports A.0, A.1, and A.2 initial state
 	
-	
-	
-	PORTA = green_light;
-	delay_in_ms(1000);
-	PORTA = yellow_light;
-	delay_in_ms(1000);
-	PORTA = red_light;
-	delay_in_ms(1000);
+	DDRG = DDRG | (1<<PG2) | (1<<PG1) | (1<<PG0);		// Ports G.0, G.1, and G.2 as output to control crosswalk signal
+	PORTG = PORTG & ~((1<<PG2) | (1<<PG1) | (1<<PG0));		// Ports A.0, A.1, and A.2 initial state
 	
 	
 	
     while (1)
     {
 		
-		
+		PORTA = green_light;
+		PORTG = dont_walk_signal;
+		delay_in_ms(10000);
+		PORTA = yellow_light;
+		delay_in_ms(4000);
+		PORTA = red_light;
+		delay_in_ms(2000);
+		PORTG = walk_signal;
+		delay_in_ms(5000);
+		for (uint8_t i = 1; i <= 10; i++)
+		{
+			PORTG = dont_walk_signal;
+			delay_in_ms(500);
+			PORTG = 0x00;
+			delay_in_ms(500);
+		}
+		PORTG = dont_walk_signal;
+		delay_in_ms(2000);
 		
     }
 }
